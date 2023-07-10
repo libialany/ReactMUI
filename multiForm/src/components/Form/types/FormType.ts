3b4240719751f14447df208c324b0ad5357b0c7e
melany.estevez <mel.estevez99@gmail.com>
2023-07-10 07:30:42 -0400

    fix: fix messy folders

--- multiForm/src/components/Form/types/FormType.ts
+++
@@ -1,4 +0,0 @@
-export interface DataFormularioType {
-  restaurantName: string;
-  address: string;
-}
--- multiForm/src/components/Form/ui/CheckoutSuccess.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CheckoutSuccess() {
-  return (
-    <div>CheckoutSuccess</div>
-  )
-}
-
-export default CheckoutSuccess
